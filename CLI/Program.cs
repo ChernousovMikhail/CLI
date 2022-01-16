ebb53e1ab17490a9593f3ce0d143694279b52139
ChernousovMikhail <67697154+ChernousovMikhail@users.noreply.github.com>
2022-01-17 00:02:15 +0300

    CLI

---
+++ CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбирите цифру:");
+            int select = Convert.ToInt32(Console.ReadLine());
+            if(select <= 50)
+            {
+                Console.WriteLine("Вариант меньше 50");
+            }
+            else
+            {
+                Console.WriteLine("Вариант больше 50");
+            }
+        }
+    }
+}
